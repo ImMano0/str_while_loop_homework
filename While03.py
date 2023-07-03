371a877a7fb5af0c1af0ee8fee72baf07026cd4f
ImMano0 <UrMa1371@gmail.com>
2023-07-03 11:12:58 +0500

    030

--- While03.py
+++ While03.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    b=0
+    c=0
+    d=len(s)
+    while a<len(s):
+        if s[a].isdigit():
+            a+=1
+        if s[a].isalpha():
+            b+=1
+        c+=1
+    return d-(b+c)
+print(main('xs$%^&BH656GG'))
