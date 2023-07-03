ee74369511ac68233633259489bf0514cee9bbc8
ImMano0 <UrMa1371@gmail.com>
2023-07-03 11:01:51 +0500

    010

--- While01.py
+++ While01.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    b=0
+    while a<len(s):
+        if s[a].isdigit():
+            b+=1
+        a+=1
+    return b
+print(main('abcde123'))
